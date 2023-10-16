d62696a515064442ae477d3c7ecf9d5acb2f853a
tanonibrenda <brendayohenatanoni@gmail.com>
2023-10-16 19:32:36 -0300

    inicio

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# importar plantillas 
-from flask import render_template
+app = Flask(__name__)
 
-#creación de aplicación
-app=Flask(__name__)
+@app.route("/")
+def principal():
+    return render_template('index.html')
 
-# Acceder a Index.html
-@app.route('/')
-def inicio():
-    return render_template('sitio/index.html')
+@app.route("/desafios")
+def desafios():
+    return render_template('desafios.html')
 
-@app.route('/templates/sitio/desafios.html')
+@app.route("/proyecto")
+def proyecto():
+    return render_template('proyecto.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/mirutadevida")
+def mirutadevida():
+    return render_template('mirutadevida.html')
+
+@app.route("/contacto")
+def contacto():
+    return render_template('contacto.html')
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
