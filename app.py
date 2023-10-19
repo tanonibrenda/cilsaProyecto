7db7af78efc8d568843700fc77fcd43ec92664cf
tanonibrenda <brendayohenatanoni@gmail.com>
2023-10-18 23:56:17 -0300

    descarga

--- app.py
+++ app.py
@@ -2,10 +2,15 @@
 import os
 from flask import Flask, render_template, request, render_template
 
+from flask import send_file
+
 from app.controllers.archivo_controller import (
-    guardar_informacion, obtener_archivos_creados, 
-    guardar_informacion_en_txt, guardar_informacion_en_word, 
-    descargar_archivo
+      
+     obtener_archivos_creados, descargar_archivo,
+     
+   
+     guardar_informacion_en_txt, guardar_informacion_en_word,
+    
 )
 
 
@@ -19,15 +24,12 @@ def principal():
 def proyecto():
     nombre_archivo = None  
     if request.method == 'POST':
-        # Obtener datos del formulario
         informacion = request.form.get('informacion')
         tipo_archivo = request.form.get('tipo_archivo')
 
         if tipo_archivo == 'bloc_notas':
-            # Guardar información en un archivo de texto (.txt)
             nombre_archivo = guardar_informacion_en_txt(informacion)
         elif tipo_archivo == 'word':
-            # Guardar información en un archivo Word (.docx)
             nombre_archivo = guardar_informacion_en_word(informacion)
 
     return render_template('proyecto.html', nombre_archivo=nombre_archivo)
@@ -35,21 +37,35 @@ def proyecto():
 
 @app.route("/ver_informacion/<archivo>")
 def ver_informacion(archivo):
+    # Ruta fija para los archivos (ajusta según tu estructura de directorios)
+    carpeta_archivos = os.path.join(app.root_path, 'archivos')
+    
     # Obtén la ruta completa del archivo
-    ruta_archivo = os.path.join(app.root_path, archivo)
+    ruta_archivo = os.path.join(carpeta_archivos, archivo)
 
     if os.path.exists(ruta_archivo):
-        with open(ruta_archivo, 'r') as file:
+        with open(ruta_archivo, 'rb') as file:
             informacion = file.read()
-        return render_template('ver_informacion.html', informacion=informacion)
+
+        # Imprime la ruta del archivo en la consola de Flask
+        app.logger.info("Ruta del archivo: %s", ruta_archivo)
+
+        return send_file(ruta_archivo, as_attachment=True)
     else:
+        app.logger.error("¡El archivo no existe! Ruta: %s", ruta_archivo)
         return "Archivo no encontrado"
     
+@app.route("/archivos_creados")
+def archivos_creados():
+    archivos = obtener_archivos_creados()
+    # Puedes renderizar una plantilla de Flask o devolver la lista de archivos en algún formato
+    return render_template('archivos_creados.html', archivos=archivos)
+
 @app.route("/descargar_archivo/<archivo>")
 def descargar(archivo):
     return descargar_archivo(archivo)
-    
-    
+
+
 @app.route("/desafios")
 def desafios():
     return render_template('desafios.html')
--- app/controllers/archivo_controller.py
+++ app/controllers/archivo_controller.py
@@ -1,7 +1,20 @@
+# archivo_controller.py
 import os
 from docx import Document
-
 from flask import send_file
+
+def guardar_informacion_en_txt(info):
+    nombre_archivo = "archivos/informacion_guardada.txt"
+    with open(nombre_archivo, "a") as archivo:
+        archivo.write(info + '\n')
+    return nombre_archivo
+
+def guardar_informacion_en_word(info):
+    nombre_archivo = "archivos/informacion_guardada.docx"
+    document = Document()
+    document.add_paragraph(info)
+    document.save(nombre_archivo)
+    return nombre_archivo
 
 def guardar_informacion_en_txt(info):
     nombre_archivo = "informacion_guardada.txt"
@@ -16,19 +29,11 @@ def guardar_informacion_en_word(info):
     document.save(nombre_archivo)
     return nombre_archivo
 
-def guardar_informacion(info, extension):
-    if extension == 'bloc_notas':
-        return guardar_informacion_en_txt(info)
-    elif extension == 'word':
-        return guardar_informacion_en_word(info)
-    else:
-        raise ValueError("Extensión no soportada")
-
 def obtener_archivos_creados():
     archivos = [f for f in os.listdir() if f.endswith("_info.txt") or f.endswith("_info.docx")]
     return archivos
 
+
 def descargar_archivo(archivo):
     ruta_archivo = os.path.join("archivos", archivo)
-    return send_file(ruta_archivo, as_attachment=True)
-
+    return send_file(ruta_archivo, as_attachment=True, mimetype="application/octet-stream")
