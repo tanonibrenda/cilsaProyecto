cabadbfa9de9ee354ff8bbe2306aeef2376d01ec
tanonibrenda <brendayohenatanoni@gmail.com>
2023-10-29 21:10:22 -0300

    faltan_datos_personales

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # app.py
 import os
 import csv
-from flask import Flask, render_template, request, send_file
+import pandas as pd
+
+from flask import Flask, render_template, request, send_file, jsonify
+from flask_caching import Cache
 
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'null'})
+df = pd.read_csv('votos.csv')
 
 # Función para cargar los datos desde el archivo CSV
 def cargar_datos(archivo):
@@ -18,9 +23,6 @@ def cargar_datos(archivo):
         print(f"Error al cargar los datos: {e}")
         return []
 
-
-
-# Función para calcular la cantidad de votos totales por partido
 def calcular_votos_totales(datos):
     votos_totales = {}
 
@@ -28,16 +30,19 @@ def calcular_votos_totales(datos):
         partido = persona.get('Partido', 'Sin Partido')
         votos_totales[partido] = votos_totales.get(partido, 0) + 1
 
+    print("Votos totales por partido:", votos_totales)  
     return votos_totales
 
-#Funcion para calcular votos por partido
-def calcular_votos_por_partido(datos):
-    votos_por_partido = {'partido1': [], 'partido2': [], 'partido3': []}
+# Funcion para calcular votos por partido
+def calcular_votos_por_partido():
+    votos_por_partido = {}
 
-    for persona in datos:
-        partido = persona.get('Partido')
+    for _, fila in df.iterrows():
+        partido = fila['Voto']
         if partido in votos_por_partido:
-            votos_por_partido[partido].append(persona)
+            votos_por_partido[partido] += 1
+        else:
+            votos_por_partido[partido] = 1
 
     return votos_por_partido
 
@@ -52,29 +57,23 @@ def guardar_gonzalez(datos):
             if persona['Apellido'] == 'Gonzalez':
                 file.write(f"{persona['Nombre']} {persona['Apellido']}\n")
 
+@cache.cached(timeout=0, key_prefix='index')
 @app.route("/")
 def principal():
-    return render_template('index.html')
+     cache.clear()
+     return render_template('index.html')
+
+# ...
+
 
 @app.route("/proyecto")
 def proyecto():
     # Tarea 1: Calcular y mostrar la cantidad de votos totales por partido
     # Cargar los datos desde el archivo CSV (votos.csv)
-    archivo_csv = os.path.join(os.path.dirname(__file__), 'votos.csv')
-    datos_votos = cargar_datos(archivo_csv)
 
-    # Calcular la cantidad de votos totales por partido
-    votos_totales = calcular_votos_totales(datos_votos)
+    # Calcular votos por partido utilizando la nueva función
+    votos_por_partido = calcular_votos_por_partido()
 
-    # Imprimir en la consola para verificar
-    print("Votos totales por partido:", votos_totales)
-    
-    #Calcular votos por partido
-    votos_por_partido = calcular_votos_por_partido(datos_votos)
-    print("Votos por Partido: ", votos_por_partido)
-    
-    
-    
     # Tarea 2: Mostrar personas con DNI mayor a 40,000,000 y nombre "Juan"
     personas_juan = filtrar_personas(datos_votos, 40000000, 'Juan')
 
@@ -82,15 +81,12 @@ def proyecto():
     personas_gonzalez = [persona for persona in datos_votos if persona['Apellido'] == 'Gonzalez']
     guardar_gonzalez(personas_gonzalez)
 
-   
-
-    return render_template('proyecto.html', 
+    # Devolver la respuesta utilizando render_template
+    return render_template('proyecto.html',
                            total_votos=len(datos_votos),
-                           votos_totales=votos_totales,
-                           votos_por_partido=votos_por_partido, 
-                           personas_juan=personas_juan, 
-                           personas_gonzalez=personas_gonzalez,
-                           )
+                           votos_por_partido=votos_por_partido,
+                           personas_juan=personas_juan,
+                           personas_gonzalez=personas_gonzalez)
 
 # Rutas a páginas accesorias
 @app.route("/integrantes")
@@ -106,4 +102,8 @@ if __name__ == "__main__":
     archivo_csv = 'votos.csv'
     datos_votos = cargar_datos(archivo_csv)
     
+    from flask_caching import Cache
+
+    cache = Cache(app, config={'CACHE_TYPE': 'null'})
+
     app.run(debug=True, host='0.0.0.0', port=5050)
